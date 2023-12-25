dace00770fe1018187b0693199aeacccc691b221
Thomé Lucas de Araújo Oliveira <chacalgbi@hotmail.com>
2023-12-25 16:21:43 -0300

    Criar tipo diversos

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module RedisConnection
-  def self.client
-    @client ||= Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
-  end
-end
+# module RedisConnection
+#   def self.client
+#     @client ||= Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
+#   end
+# end
