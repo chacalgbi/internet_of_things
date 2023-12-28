3ecb560bcd227556a6d3fb67d7c70a51b7b6b6f4
Thomé Lucas de Araújo Oliveira <chacalgbi@hotmail.com>
2023-12-28 10:57:29 -0300

    Logs redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,8 +2,13 @@
 
 module RedisConnection
   def self.client
-    Log.info('Redis: Conectando ao Redis...')
-    Log.info("Redis: host: #{ENV['REDIS_HOST']} - port: #{ENV['REDIS_PORT']}")
-    @client ||= Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
+    begin
+      Log.info('Redis: Conectando ao Redis...')
+      Log.info("Redis: host: #{ENV['REDIS_HOST']} - port: #{ENV['REDIS_PORT']}")
+      @client ||= Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
+    rescue StandardError => e
+      Log.error("Redis: Erro ao conectar: #{e.message}")
+      Log.error(e.backtrace.join("\n"))
+    end
   end
 end
