f6d7003de4ce48c3561e6c6a6f72c820ea1609ec
Thomé Lucas de Araújo Oliveira <chacalgbi@hotmail.com>
2023-12-28 10:49:03 -0300

    Change logs

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,8 @@
 
 module RedisConnection
   def self.client
+    Log.info('Redis: Conectando ao Redis...')
+    Log.info("Redis: host: #{ENV['REDIS_HOST']} - port: #{ENV['REDIS_PORT']}")
     @client ||= Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
   end
 end
